dfd1a872d1b5c939962689eb2519bad80ddb7ebc
Anas Yousif <Anasyousif182@gmail.com>
2025-03-04 20:08:49 -0500

    what

---
+++ PalindromeSubstrings.java
@@ -0,0 +1,11 @@
+public class PalindromeSubstrings {
+    public static int countPalindromeSubstrings(String s) {
+        // Your code here
+    }
+
+    public static void main(String[] args) {
+        String test = "aaa";
+        System.out.println(countPalindromeSubstrings(test));
+    }
+}
+
