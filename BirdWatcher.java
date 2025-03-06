3460aebc9e7197093b655669aaa186381ca2e9ac
Anas Yousif <Anasyousif182@gmail.com>
2025-03-05 19:29:12 -0500

    bird

---
+++ BirdWatcher.java
@@ -0,0 +1,49 @@
+class BirdWatcher {
+    private final int[] birdsPerDay;
+    private static final int[] LAST_WEEK_COUNTS = {0, 2, 5, 3, 7, 8, 4};
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return LAST_WEEK_COUNTS.clone();
+    }
+
+    public int getToday() {
+        return birdsPerDay.length > 0 ? birdsPerDay[birdsPerDay.length - 1] : 0;
+    }
+
+    public void incrementTodaysCount() {
+        if (birdsPerDay.length > 0) {
+            birdsPerDay[birdsPerDay.length - 1]++;
+        }
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for (int count : birdsPerDay) {
+            if (count == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int sum = 0;
+        for (int i = 0; i < Math.min(numberOfDays, birdsPerDay.length); i++) {
+            sum += birdsPerDay[i];
+        }
+        return sum;
+    }
+
+    public int getBusyDays() {
+        int busyDays = 0;
+        for (int count : birdsPerDay) {
+            if (count >= 5) {
+                busyDays++;
+            }
+        }
+        return busyDays;
+    }
+}
