74e1a8489cada1860adbfafba3ff3474d8c6b8d0
Anas Yousif <Anasyousif182@gmail.com>
2025-05-19 15:40:49 -0400

    cars

---
+++ needsLicense.js
@@ -0,0 +1,26 @@
+export function needsLicense(kind) {
+  if (kind=== "car" || kind==="truck") {
+    return true; 
+  }
+  else {
+    return false;
+  }
+}
+
+
+export function chooseVehicle(option1, option2) {
+  const better = option1 < option2 ? option1 : option2;
+  return `${better} is clearly the better choice.`;
+}
+
+export function calculateResellPrice(originalPrice, age) {
+  if (age <3) {
+    return originalPrice * 0.80
+  }
+  else if (age > 10 ){
+    return originalPrice * 0.50
+  }
+  else {
+    return originalPrice * 0.70
+  }
+}
