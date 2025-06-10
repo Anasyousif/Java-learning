9b59e1f7accc01f5c7a01009cb6407b6f50552bf
Anas Yousif <Anasyousif182@gmail.com>
2025-06-10 18:07:09 -0400

    Permatations

---
+++ Permutations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Permutations {
+    
+    public static List<String> singlePermutations(String s) {
+        Set<String> result = new HashSet<>();
+        permute("", s, result);
+        return new ArrayList<>(result);
+    }
+    
+    private static void permute(String prefix, String remaining, Set<String> result) {
+        if (remaining.isEmpty()) {
+            result.add(prefix);
+        } else {
+            for (int i = 0; i < remaining.length(); i++) {
+                String newPrefix = prefix + remaining.charAt(i);
+                String newRemaining = remaining.substring(0, i) + remaining.substring(i + 1);
+                permute(newPrefix, newRemaining, result);
+            }
+        }
+    }
+}
