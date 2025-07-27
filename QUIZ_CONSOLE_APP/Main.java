d4af4c88944ccc542ce7a49b2594197147983aba
Anas Yousif <Anasyousif182@gmail.com>
2025-07-26 20:28:32 -0400

    adding

--- QUIZ_CONSOLE_APP/Main.java
+++ QUIZ_CONSOLE_APP/Main.java
@@ -3,6 +3,6 @@ package QUIZ_CONSOLE_APP;
 public class Main {
     public static void main(String[] args) {
         QuestionService service = new QuestionService();
-        service.displayQuestions();
+        service.PlayQuiz();
     }
 }
--- QUIZ_CONSOLE_APP/QuestionService.java
+++ QUIZ_CONSOLE_APP/QuestionService.java
@@ -3,12 +3,21 @@ package QUIZ_CONSOLE_APP;
 public class QuestionService {
     Question[]  questions = new Question[5];
 
-    public void displayQuestions(){
+    public QuestionService(){
         questions[0] = new Question(1,"What","Java","Python","C","C++","Java");
         questions[1] = new Question(2, "size of double", "2", "6", "4", "8", "8");
         questions[2] = new Question(3, "size of char", "2", "6", "4", "8", "2");
         questions[3] = new Question(4, "size of long", "2", "6", "4", "8", "8");
         questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
-        System.out.println(questions[0]);
     }
+
+    public void PlayQuiz(){
+        for(Question q: questions){
+            System.out.println("Question no. :"+q.getId());
+            System.out.println(q.getQuestion());
+        }
+
+    }
+
+    
 }
